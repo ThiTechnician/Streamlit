9cc53a9eca572c6831e427e82b29a56c57e51c20
Thiago Martins Carvalho de Medeiros <82727853+ThiTechnician@users.noreply.github.com>
2024-08-31 15:25:13 -0300

    Update Apresentação.py

--- Apresentação.py
+++ Apresentação.py
@@ -4,12 +4,18 @@ st.markdown("""
 ## Sobre Mim
 
 <p style="text-align: justify;">
-Com mais de uma década de experiência na área de tecnologia gráfica, tenho me dedicado a expandir e aprimorar meu conhecimento em Ciência de Dados. Sou formado em Engenharia de Produção e possuo uma pós-graduação em Ciência de Dados, além de uma sólida experiência em programação com Python. 
+Com mais de uma década de experiência na área de tecnologia gráfica, tenho me dedicado a expandir e aprimorar meu conhecimento em Ciência de Dados. Sou formado em Engenharia de Produção e possuo uma pós-graduação em Ciência de Dados, além de uma sólida experiência em programação com Python.
+</p>
 
+<p style="text-align: justify;">
 Minha jornada inclui o desenvolvimento de aplicativos e soluções para análise e visualização de dados, utilizando ferramentas como Streamlit e Plotly, além de trabalhar com SQL e MySQL para manipulação e análise de grandes volumes de dados.
+</p>
 
+<p style="text-align: justify;">
 Recentemente, tenho me concentrado na criação de dashboards interativos e geração de relatórios em PDF para clientes, integrando gráficos avançados e insights valiosos. Também sou familiarizado com o uso de bibliotecas como Pandas para manipulação de dados e ferramentas de autenticação como Streamlit Authenticator para garantir a segurança e personalização do acesso em aplicativos web.
+</p>
 
+<p style="text-align: justify;">
 Possuo habilidades em linguagens adicionais como C, HTML e CSS, e estou habituado ao uso do Git para versionamento e colaboração em projetos. Estou constantemente em busca de novas oportunidades para aplicar e expandir meu conhecimento em Ciência de Dados, aproveitando minhas habilidades técnicas e conhecimentos adquiridos ao longo da graduação e da experiência prática.
 </p>
 """, unsafe_allow_html=True)
