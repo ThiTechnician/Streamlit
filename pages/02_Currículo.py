f04cc6865aaa1cd97c8d7c79a8f72f6b5177bad2
Thiago Martins Carvalho de Medeiros <82727853+ThiTechnician@users.noreply.github.com>
2024-08-31 14:20:37 -0300

    Update 02_Currículo.py

--- pages/02_Currículo.py
+++ pages/02_Currículo.py
@@ -1,8 +1,21 @@
 import streamlit as st
 
 # Especifica o nome do arquivo PDF no mesmo diretório do script
-pdf_file = "pages/curriculo_linkedin.pdf"
+pdf_file = "curriculo_linkedin.pdf"
+
+# CSS para tornar o iframe responsivo
+css = """
+<style>
+iframe {
+    width: 100vw; /* Ajusta a largura para 100% da largura da tela */
+    height: 100vh; /* Ajusta a altura para 100% da altura da tela */
+    border: none; /* Remove a borda do iframe */
+}
+</style>
+"""
 
 # Exibe o PDF usando um iframe
-pdf_display = f'<iframe src="{pdf_file}" width="700" height="1000" type="application/pdf"></iframe>'
-st.markdown(pdf_display, unsafe_allow_html=True)
+pdf_display = f'<iframe src="{pdf_file}" type="application/pdf"></iframe>'
+
+# Aplica o CSS e exibe o PDF
+st.markdown(css + pdf_display, unsafe_allow_html=True)
