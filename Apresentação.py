5bcf36fb05492e7db670ed6c94e95d5bc13acc51
Thiago Martins Carvalho de Medeiros <82727853+ThiTechnician@users.noreply.github.com>
2024-08-31 15:14:52 -0300

    Update Apresentação.py

--- Apresentação.py
+++ Apresentação.py
@@ -1,1 +1,10 @@
 import streamlit as st
+
+st.title('Sobre mim')
+st.text('
+
+Com mais de uma década de experiência na área de tecnologia gráfica, tenho me dedicado a expandir e aprimorar meu conhecimento em Ciência de Dados. Sou formado em Engenharia de Produção e possuo uma pós-graduação em Ciência de Dados, além de uma sólida experiência em programação com Python. Minha jornada inclui o desenvolvimento de aplicativos e soluções para análise e visualização de dados, utilizando ferramentas como Streamlit e Plotly, além de trabalhar com SQL e MySQL para manipulação e análise de grandes volumes de dados.
+
+Recentemente, tenho me concentrado na criação de dashboards interativos e geração de relatórios em PDF para clientes, integrando gráficos avançados e insights valiosos. Também sou familiarizado com o uso de bibliotecas como Pandas para manipulação de dados e ferramentas de autenticação como Streamlit Authenticator para garantir a segurança e personalização do acesso em aplicativos web.
+
+Possuo habilidades em linguagens adicionais como C, HTML e CSS, e estou habituado ao uso do Git para versionamento e colaboração em projetos. Estou constantemente em busca de novas oportunidades para aplicar e expandir meu conhecimento em Ciência de Dados, aproveitando minhas habilidades técnicas e conhecimentos adquiridos ao longo da graduação e da experiência prática.')
