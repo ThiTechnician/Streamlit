d4f8fd4165282cb14f67e4ba0ee81f15f08beede
Thiago Martins Carvalho de Medeiros <82727853+ThiTechnician@users.noreply.github.com>
2024-08-31 15:17:09 -0300

    Update Apresentação.py

--- Apresentação.py
+++ Apresentação.py
@@ -7,4 +7,6 @@ Com mais de uma década de experiência na área de tecnologia gráfica, tenho m
 
 Recentemente, tenho me concentrado na criação de dashboards interativos e geração de relatórios em PDF para clientes, integrando gráficos avançados e insights valiosos. Também sou familiarizado com o uso de bibliotecas como Pandas para manipulação de dados e ferramentas de autenticação como Streamlit Authenticator para garantir a segurança e personalização do acesso em aplicativos web.
 
-Possuo habilidades em linguagens adicionais como C, HTML e CSS, e estou habituado ao uso do Git para versionamento e colaboração em projetos. Estou constantemente em busca de novas oportunidades para aplicar e expandir meu conhecimento em Ciência de Dados, aproveitando minhas habilidades técnicas e conhecimentos adquiridos ao longo da graduação e da experiência prática.')
+Possuo habilidades em linguagens adicionais como C, HTML e CSS, e estou habituado ao uso do Git para versionamento e colaboração em projetos. Estou constantemente em busca de novas oportunidades para aplicar e expandir meu conhecimento em Ciência de Dados, aproveitando minhas habilidades técnicas e conhecimentos adquiridos ao longo da graduação e da experiência prática.
+
+        ')
