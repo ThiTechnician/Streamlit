cc57c7281894f3ffb8a3f17249302470c74782a0
Thiago Martins Carvalho de Medeiros <82727853+ThiTechnician@users.noreply.github.com>
2024-08-31 14:04:15 -0300

    Adicionada visualização de pdf

--- pages/03_LinkedIn.py
+++ pages/03_LinkedIn.py
@@ -1,7 +1,3 @@
 import streamlit as st
 
-# URL da página web que você deseja exibir
-url = "https://www.linkedin.com/in/engenheirothiagomedeiros"
-
-# Exibe a página web dentro de um iframe
-st.components.v1.iframe(url, width=800, height=600, scrolling=True)
+st.pdf('curriculo_linkedin.pdf)
