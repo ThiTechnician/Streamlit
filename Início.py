dd095f0ee1e08d027bcb534fd587272b88e5f830
Thiago Martins Carvalho de Medeiros <82727853+ThiTechnician@users.noreply.github.com>
2024-08-31 21:45:05 -0300

    Update Início.py

--- Início.py
+++ Início.py
@@ -1,1 +1,17 @@
-import streamlit as st 
+
+import streamlit as st
+
+st.markdown(
+    """
+    <div style="text-align: justify;">
+    **Apresentação**
+    
+    Bem-vindo ao meu portfólio online! Este espaço foi criado para compartilhar minha trajetória, projetos, e conhecimentos na área de [sua área de atuação]. Aqui, você encontrará informações sobre minha formação, experiências profissionais, e os trabalhos que desenvolvi ao longo da minha carreira.
+    
+    O objetivo deste site é oferecer uma visão abrangente das minhas habilidades e contribuições em diversos projetos. Explore as seções dedicadas aos meus **Projetos de Análise** para entender como aplico metodologias e ferramentas analíticas em situações reais, além de outras iniciativas que refletem meu compromisso com a inovação e a excelência.
+    
+    Sinta-se à vontade para navegar pelas diferentes páginas e conhecer mais sobre mim e o que posso oferecer. Se desejar entrar em contato para discussões, colaborações ou apenas para trocar ideias, a seção de **Contato** está à sua disposição.
+    </div>
+    """, 
+    unsafe_allow_html=True
+)
