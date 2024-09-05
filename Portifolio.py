c0a10f8276aa105df78f630e1616a0bbafbe19d8
Thiago Martins Carvalho de Medeiros <82727853+ThiTechnician@users.noreply.github.com>
2024-09-04 22:45:07 -0300

    Update Portifolio.py

--- Portifolio.py
+++ Portifolio.py
@@ -1,1 +1,14 @@
+import streamlit as st
+import plotly.express as px
 
+# Exemplo de DataFrame
+import pandas as pd
+data = {'Categoria': ['A', 'B', 'C', 'D'], 
+        'Valor': [10, 20, 15, 30]}
+df = pd.DataFrame(data)
+
+# Plotando o gráfico com Plotly Express
+fig = px.bar(df, x='Categoria', y='Valor', color='Categoria')
+
+# Exibindo o gráfico no Streamlit
+st.plotly_chart(fig)
