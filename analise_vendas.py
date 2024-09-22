b6fab9720430c520e7cb09848dcddbfa3680a1fb
Thiago Martins Carvalho de Medeiros <82727853+ThiTechnician@users.noreply.github.com>
2024-09-22 13:01:19 -0300

    Update analise_vendas.py

--- analise_vendas.py
+++ analise_vendas.py
@@ -9,8 +9,12 @@ st.title("Análise de Dados de Vendas")
 uploaded_file = st.file_uploader("Carregar arquivo de dados de vendas (CSV)", type=["csv"])
 
 if uploaded_file is not None:
-    # Ler o arquivo CSV
-    df = pd.read_csv(uploaded_file)
+    try:
+        # Tentar ler o arquivo CSV com codificação ISO-8859-1
+        df = pd.read_csv(uploaded_file, encoding="ISO-8859-1")
+    except UnicodeDecodeError:
+        st.error("Erro ao ler o arquivo. Tente usar um arquivo com codificação UTF-8 ou ISO-8859-1.")
+
 
     # Exibir os dados carregados
     st.write("Visualização dos dados de vendas:")
