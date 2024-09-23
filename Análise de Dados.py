1783694c90379cab4d63c9bfc296c651eed28e68
Thiago Martins Carvalho de Medeiros <82727853+ThiTechnician@users.noreply.github.com>
2024-09-22 23:58:19 -0300

    Update Análise de Dados.py

--- Análise de Dados.py
+++ Análise de Dados.py
@@ -1,1 +1,10 @@
 import streamlit as st
+
+# URL pública do notebook do Google Colab
+colab_url = "import streamlit as st
+
+# URL pública do notebook do Google Colab
+colab_url = "https://colab.research.google.com/drive/1hUdU3XB8FsPLW1QybYYuhF5cl64llmuw?usp=sharing"  # Substitua pelo link gerado
+
+# Exibe o notebook usando um iframe
+st.components.v1.iframe(colab_url, width=800, height=600)
